699104b863d9a7e39515470eda19d062403af228
mugambi484 <123549183+mugambi484@users.noreply.github.com>
2024-04-07 18:11:00 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function returnCustomerName() {
+    return customerName;
+}
+function upperCaseCustomerName() {
+    customerName = customerName.toLowerCase(); 
+    customerName = customerName.charAt(0).toUpperCase() + customerName.slice(1);
+}
+var bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+let leastFavoriteCustomer = 'initial least favorite customer';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new least favorite customer';
+}
+
+
+
